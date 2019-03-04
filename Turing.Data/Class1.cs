846eafd65d3b368897d322c04281eb8d01259e57
Timileyin <timileyinayegbayo@gmail.com>
2019-03-04 16:43:40 +0100

    Add project files.

---
+++ Turing.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Turing.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ Turing.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Turing.Repository
+{
+    public class Class1
+    {
+    }
+}
